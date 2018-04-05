cc77bf45dcf98673cc52645aed95b5d5a6cf0588
Hee Kang <kanglm32@gmail.com>
2018-04-04 18:19:55 -0700

    lateish

---
+++ Skeleton/wip.js
@@ -0,0 +1,112 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {top: 20, right: 40, bottom: 60, left: 100};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select('.chart')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+var chart = svg.append('g');
+
+d3.csv('data.csv', function(err, corrData) {
+  if (err) throw err;
+
+  corrData.forEach(function(data) {
+    data.children = +data.children;
+    data.uninsured = +data.uninsured;
+  });
+
+  // Create scale functions
+  var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+  var xLinearScale = d3.scaleLinear().range([0, width]);
+
+  // Create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Scale the domain
+  xLinearScale.domain([
+    20,
+    d3.max(corrData, function(data) {
+      return +data.children;
+    }),
+  ]);
+  yLinearScale.domain([
+    0,
+    d3.max(corrData, function(data) {
+      return +data.uninsured * 1.2;
+    }),
+  ]);
+
+  var toolTip = d3
+    .tip()
+    .attr('class', 'tooltip')
+    .offset([80, -60])
+    .html(function(data) {
+      var stateName = data.locationa;
+      var childrens = +data.children;
+      var uninsureds = +data.uninsured;
+      return (
+        stateName
+      );
+    });
+
+  chart.call(toolTip);
+
+  chart
+    .selectAll('circle')
+    .data(hairData)
+    .enter()
+    .append('circle')
+    .attr('cx', function(data, index) {
+      return xLinearScale(data.children);
+    })
+    .attr('cy', function(data, index) {
+      return yLinearScale(data.uninsured);
+    })
+    .attr('r', '15')
+    .attr('fill', 'pink')
+    .on('click', function(data) {
+      toolTip.show(data);
+    })
+    // onmouseout event
+    .on('mouseout', function(data, index) {
+      toolTip.hide(data);
+    });
+
+  chart
+    .append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chart.append('g').call(leftAxis);
+
+  chart
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 0 - margin.left + 40)
+    .attr('x', 0 - height / 2)
+    .attr('dy', '1em')
+    .attr('class', 'axisText')
+    .text('Number of Billboard 100 Hits');
+
+  // Append x-axis labels
+  chart
+    .append('text')
+    .attr(
+      'transform',
+      'translate(' + width / 2 + ' ,' + (height + margin.top + 30) + ')',
+    )
+    .attr('class', 'axisText')
+    .text('Hair Metal Band Hair Length (inches)');
+});
